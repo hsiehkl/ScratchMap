dbb4bee5eeabab74aa8bfa1e2f2c7ed3ad9d9e21
hsiehkl <st4465@gmail.com>
2018-01-04 14:58:12 +0800

    Update country list

--- ScratchMap/Model/CountryList.swift
+++ ScratchMap/Model/CountryList.swift
@@ -10,11 +10,11 @@ import Foundation
 
 let countryIdClassifiedByContinents = [
     
-    "Europe": ["AL", "AD", "AM", "AT", "AX", "AZ", "BY", "BE", "BA", "BG", "HR", "CY", "CZ", "DK", "EE", "FI", "FR", "GE", "DE", "GR", "HU", "IS", "IE", "IT", "XK", "LV", "LI", "LT", "LU", "MK", "MT", "MD", "MC", "ME", "NL", "NO", "PL", "PT", "RO", "RU", "SM", "RS", "SK", "SI", "ES", "SE", "CH", "TR", "UA", "GB", "VA"],
+    "Europe": ["AL", "AD", "AM", "AT", "AX", "AZ", "BY", "BE", "BA", "BG", "HR", "CY", "CZ", "DK", "EE", "FI", "FO", "FR", "GE", "GE", "GI", "DE", "GR", "HU", "IM","IS", "IE", "JE", "IT", "XK", "LV", "LI", "LT", "LU", "MK", "MT", "MD", "MC", "ME", "NL", "NO", "PL", "PT", "RO", "RU", "SM", "RS", "SK", "SJ", "SI", "ES", "SE", "CH", "TR", "UA", "GB", "VA"],
     
-    "Asia": ["AF", "AM" ,"AZ", "BH", "BD", "BT", "BN", "KH", "CN", "CY", "GE", "IN","ID", "IR", "IQ", "IL", "JP", "JO", "KZ", "KW", "KG", "LA", "LB", "MY", "MV", "MN", "MM", "NP", "KP", "OM", "PK", "PS", "PH", "QA", "RU", "SA", "SG", "KR", "LK", "SY", "TW", "TJ", "TH", "TL", "TR", "TM", "AE", "UZ", "VN", "YE"],
+    "Asia": ["AF", "AM" ,"AZ", "BH", "BD", "BT", "BN", "KH", "CN", "CY", "CX", "GE", "IN","ID", "IR", "IQ", "IL", "JP", "JO", "KZ", "KW", "KG", "LA", "LB", "MY", "MV", "MN", "MM", "NP", "KP", "OM", "PK", "PS", "PH", "QA", "RU", "SA", "SG", "KR", "LK", "SY", "TW", "TJ", "TH", "TL", "TR", "TM", "AE", "UZ", "VN", "YE"],
     
-    "Africa": [ "DZ", "AO", "BJ", "BW", "BF", "BI", "CV", "CM", "CF", "TD", "KM", "CD", "CG", "CI", "DJ", "EG", "GQ", "ER", "ET", "GA", "GM", "GH", "GN", "GW", "KE", "LS", "LR", "LY", "MG", "MW", "ML", "MR", "MU", "MA", "MZ", "NA", "NE", "NG", "RW", "ST", "SN", "SC", "SL", "SO", "ZA", "SS", "SD", "SZ", "TZ", "TG", "TN", "UG", "ZM", "ZW"],
+    "Africa": [ "DZ", "AO", "BJ", "BW", "BF", "BI", "CV", "CM", "CF", "TD", "KM", "CD", "CG", "CI", "DJ", "EG", "GQ", "ER", "ET", "GA", "GM", "GH", "GN", "GW", "KE", "LS", "LR", "LY", "MG", "MW", "ML", "MR", "MU", "MA", "MZ", "NA", "NE", "NG", "RE", "RW", "ST", "SN", "SC", "SL", "SO", "ZA", "SS", "SD", "SZ", "TZ", "TG", "TN", "UG", "YT","ZM", "ZW"],
     
     "North America" : ["AG", "BS", "BB", "BZ", "CA", "CR", "CU", "DM", "DO", "SV", "GD", "GT", "HT", "HN", "JM", "MX", "NI", "PA", "KN", "LC", "VC", "TT", "US", "AI", "AW", "BM", "BQ", "VG", "KY", "CW", "GL", "GP", "MQ", "MS", "PR", "BQ", "BL", "MF", "PM", "SX", "TC", "VI"],
     
